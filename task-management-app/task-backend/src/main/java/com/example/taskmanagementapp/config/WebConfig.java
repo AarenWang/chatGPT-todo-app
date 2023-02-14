b60464441552999ed871e104ec572900f30ef34c
AarenWang <AarenWang@github.com>
2023-02-14 21:14:19 +0800

    调整 pom.xml

---
+++ task-management-app/task-backend/src/main/java/com/example/taskmanagementapp/config/WebConfig.java
@@ -0,0 +1,10 @@
+package com.example.taskmanagementapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig {
+    // Add any additional web-related configuration here
+}
--- task-management-app/task-backend/src/main/java/com/example/taskmanagementapp/service/TaskService.java
+++ task-management-app/task-backend/src/main/java/com/example/taskmanagementapp/service/TaskService.java
@@ -1,48 +1,13 @@
 package com.example.taskmanagementapp.service;
 
 import com.example.taskmanagementapp.model.Task;
-import com.example.taskmanagementapp.repository.TaskRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class TaskServiceImpl implements TaskService {
-
-    @Autowired
-    private TaskRepository taskRepository;
-
-    @Override
-    public List<Task> getAllTasks() {
-        return taskRepository.findAll();
-    }
-
-    @Override
-    public Task getTaskById(Long id) {
-        return taskRepository.findById(id).orElse(null);
-    }
-
-    @Override
-    public Task createTask(Task task) {
-        return taskRepository.save(task);
-    }
-
-    @Override
-    public Task updateTask(Long id, Task task) {
-        Task existingTask = taskRepository.findById(id).orElse(null);
-        if (existingTask == null) {
-            return null;
-        }
-        existingTask.setName(task.getName());
-        existingTask.setBeginTime(task.getBeginTime());
-        existingTask.setEndTime(task.getEndTime());
-        existingTask.setCompleted(task.isCompleted());
-        return taskRepository.save(existingTask);
-    }
-
-    @Override
-    public void deleteTask(Long id) {
-        taskRepository.deleteById(id);
-    }
+public interface TaskService {
+    List<Task> getAllTasks();
+    Task getTaskById(Long id);
+    Task createTask(Task task);
+    Task updateTask(Long id, Task task);
+    void deleteTask(Long id);
 }
