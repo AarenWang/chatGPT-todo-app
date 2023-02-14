0969518ab7125605442b2a56b949c688e760bf84
AarenWang <AarenWang@github.com>
2023-02-14 20:48:59 +0800

    改用MyBatis

--- task-management-app/task-backend/src/main/java/com/example/taskmanagementapp/repository/TaskRepository.java
+++ task-management-app/task-backend/src/main/java/com/example/taskmanagementapp/repository/TaskRepository.java
@@ -1,7 +1,28 @@
 package com.example.taskmanagementapp.repository;
 
 import com.example.taskmanagementapp.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.apache.ibatis.annotations.*;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
+import java.util.List;
+
+@Mapper
+public interface TaskRepository {
+
+    @Select("SELECT * FROM task")
+    List<Task> findAll();
+
+    @Select("SELECT * FROM task WHERE id = #{id}")
+    Task findById(Long id);
+
+    @Insert("INSERT INTO task (name, begin_time, end_time, completed) " +
+            "VALUES (#{name}, #{beginTime}, #{endTime}, #{completed})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void insert(Task task);
+
+    @Update("UPDATE task SET name = #{name}, begin_time = #{beginTime}, " +
+            "end_time = #{endTime}, completed = #{completed} WHERE id = #{id}")
+    void update(Task task);
+
+    @Delete("DELETE FROM task WHERE id = #{id}")
+    void deleteById(Long id);
 }
